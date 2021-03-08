79ef2ff61c10a0d197cb526c23d12a288288908a
nrd141913 <nrd141913@gmail.com>
2021-03-08 12:17:16 +0600

    done

--- src/components/Showdetails/Showdetails.js
+++ src/components/Showdetails/Showdetails.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 
 const Showdetails = () => {
     const {id} = useParams();
+
     const [posts, setPosts] = useState({});
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
@@ -12,11 +13,25 @@ const Showdetails = () => {
     },[])
 
     const {title, body} = posts;
+
+    const [comments, setcomments] = useState([]);
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/comments/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setcomments(data))
+    },[])
+
+    const {name, email,} = comments;
+
+    
     
     return (
         <div>
            <h2>Show details for id {id}</h2> 
            <h1>{title}</h1>
+           <h4>Name: {name}</h4>
+           <h5>Email: {email}</h5>
            <p>{body}</p>
         </div>
     );
